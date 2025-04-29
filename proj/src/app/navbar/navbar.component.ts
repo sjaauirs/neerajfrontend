83387a20e918c8a85e5d7492daf27726b97ffc33
saurabh <saurabh28jaiswal@gmail.com>
2025-04-29 10:53:54 +0530

    Adjust NavBar

--- proj/src/app/navbar/navbar.component.ts
+++ proj/src/app/navbar/navbar.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
-
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   standalone : true
 })
 export class NavbarComponent {
 
-  isAuthenticated = false;
+  isMenuOpen = false;
 
   constructor() {}
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
   async ngOnInit() {
     // Check if user is already logged in
-
-    
-
 }
 }
